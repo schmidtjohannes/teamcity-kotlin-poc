abeec1df078e8f3fd32d2a2165d0ebb387020d85
Johannes Schmidt <johannes.schmidt.delaunay@gmail.com>
2018-05-16 17:29:10 -0300

    add project test

---
+++ Test/Project.kt
@@ -0,0 +1,11 @@
+package Test
+
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+
+object Project : Project({
+    uuid = "06c980d8-49cf-46b6-abc8-7e6c9f81746a"
+    id = "Test"
+    parentId = "_Root"
+    name = "test"
+})
