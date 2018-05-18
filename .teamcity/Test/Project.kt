e7194d31f31865fe301f07d68ed3ab687132f34b
Johannes Schmidt <johannes.schmidt.delaunay@gmail.com>
2018-05-18 14:30:09 -0300

    add test project

---
+++ .teamcity/Test/Project.kt
@@ -0,0 +1,11 @@
+package Test
+
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+
+object Project : Project({
+    uuid = "b7d31b58-7ea4-456f-bf19-49647b7c1064"
+    id = "Test"
+    parentId = "_Root"
+    name = "Test"
+})
