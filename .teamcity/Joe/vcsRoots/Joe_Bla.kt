7a44eb9dd95c5bf21cb1453366724c4880835c5c
Johannes Schmidt <johannes.schmidt.delaunay@gmail.com>
2018-05-18 11:11:40 -0300

    mv vcs

--- .teamcity/Joe/vcsRoots/Joe_Bla.kt
+++ .teamcity/Joe/vcsRoots/Joe_Bla.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
 
 object Test_Bla : GitVcsRoot({
     uuid = "d7649b4d-65f3-4d14-a08a-ff1ac9d03238"
-    id = "Test_Bla"
+    id = "Joe_Bla"
     name = "bla"
     url = "https://github.com/schmidtjohannes/teamcity-kotlin-poc.git"
     authMethod = password {
